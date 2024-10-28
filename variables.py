0710ba6101590a2a50215c0a65dadcb59276a440
wyreanderson <wyreanderson@gmail.com>
2024-10-28 08:12:33 +0000

    Learning variables

---
+++ variables.py
@@ -0,0 +1,38 @@
+#string
+name = "Wyre"
+color = "Red"
+favorite_food = "Nordestina"
+
+print(name)
+print(color)
+print(favorite_food)
+
+
+#integers
+age = 24
+jobs = 1
+random_number = 4
+
+print(age)
+print(jobs)
+print(random_number)
+
+
+#float
+
+wallet = 34.54
+hour = 08.04
+day = 28.10
+
+print(wallet)
+print(hour)
+print(day)
+
+
+#boolean
+is_bald = False
+
+if is_bald == True:
+    print(f"You are bald {name}")
+else:
+    print("Nice haircut")
