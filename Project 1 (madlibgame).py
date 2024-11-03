86895602560739f76d5d68e1a9efc00b353a2f65
wyreanderson <wyreanderson@gmail.com>
2024-11-03 12:37:04 +0000

    First project

---
+++ Project 1 (madlibgame).py
@@ -0,0 +1,11 @@
+print("***Uma Aventura na Cozinha:***\n")
+print("Instruções: O Madlib é um passatempo para criar histórias onde cada participante escolhe uma palavra.")
+print("Regras: Insira as palavras de acordo como é pedido nas caixinhas. \n")
+
+nome = input("Qual nome do personagem?: ")
+receita = input("Escolha uma receita: ")
+lanche = input("Escolha um lanche: ")
+comida = input("Escolha uma comida: ")
+
+
+print(f" {nome} adorava ajudar a mãe na cozinha. Hoje, enquanto ela preparava o {receita} favorito dele, ele aproveitou para fazer uma descoberta incrível: um {lanche} escondido na prateleira mais alta. Com muito cuidado, pegou uma escadinha e saboreou o {lanche}. Mas, ao se distrair com tanto sabor, derrubou um pouco de {comida}. Rindo da própria bagunça, {nome} ajudou a mãe a limpar tudo e, no final, saboreou o {receita} mais delicioso do mundo, com um sorriso de orelha a orelha.")
