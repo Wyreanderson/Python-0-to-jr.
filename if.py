12e22315a6bf3c52f0616b4259361b42b0f045d6
wyreanderson <wyreanderson@gmail.com>
2024-11-03 23:13:39 +0000

    if statements

---
+++ if.py
@@ -0,0 +1,9 @@
+# if = Do some code onle IF some condition is True
+#      Do something else
+
+age = int(input('Enter your age: '))
+
+if age >= 18:
+    print("You can drive!")
+else: 
+    print("You can't drive!")
