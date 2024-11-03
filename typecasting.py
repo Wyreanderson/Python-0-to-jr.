45b3df608db5b8aaf15fbc380454b4e7bf76bc5c
wyreanderson <wyreanderson@gmail.com>
2024-11-03 11:28:30 +0000

    My var study

---
+++ typecasting.py
@@ -0,0 +1,19 @@
+#Typecasting = The process of converting a variable from one data type to another.
+#       str(), int(), float(), bool()
+
+
+#str() to bool
+name = "Wyre"
+print(type(name))
+
+name = bool(name)
+print(type(name))
+
+
+#int() to float()
+age = 24
+print(type(age))
+
+age = float(age)
+print(type(age))
+
