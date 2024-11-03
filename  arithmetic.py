0353005c8141a76d470ab59387b88c476a94410d
wyreanderson <wyreanderson@gmail.com>
2024-11-03 22:59:00 +0000

    math

---
+++  arithmetic.py
@@ -0,0 +1,25 @@
+friends = 10
+
+#friends = friends - 1
+friends -= 1
+print(friends)
+
+#friends = friends + 1
+friends += 1
+print(friends)
+
+#friends = friends / 2
+friends /= 2
+print(friends)
+
+#friends = friends *2
+friends *= 2
+print(friends)
+
+#friends = friends **2
+friends **= 2
+print(friends)
+
+#friends = friends % 3
+friends %= 3
+print(friends)
