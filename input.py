cb252366fd2181d11b56b2756490159bc721a25a
wyreanderson <wyreanderson@gmail.com>
2024-11-03 11:55:41 +0000

    input exemples

---
+++ input.py
@@ -0,0 +1,9 @@
+# input() = A function that prompts the user to enter data
+#           Returns the entered data as a string
+
+
+#Exemples 
+
+name = input("What's is your name?: ")
+age = input("What's is your age?: ")
+email = input("Insert your email: ")
